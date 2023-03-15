caed1ed4297813d43199fa84ea248f141b7ba8db
Dmitriy Tihomirov <dmitriy.tihomirov@okdesk.ru>
2023-03-15 14:29:50 +0300

    add limit

--- main.rb
+++ main.rb
@@ -3,7 +3,18 @@ require_relative 'dadata_api'
 require 'json'
 require_relative 'csv_writer'
 
+LIMIT_REQUESTS = 180000
+
 def run
+  limit_data = JSON.parse(File.read('limit.json'))
+  if limit_data['date'] != Date.today.to_s
+    limit_data['date'] = Date.today.to_s
+    limit_data['request_count'] = 0
+  end
+  if limit_data['request_count'] >= LIMIT_REQUESTS
+    return
+  end
+
   output_inns = JSON.parse(File.read('output.json'))
   processed_inns = JSON.parse(File.read('processed.json')) || []
   inns = output_inns - processed_inns
@@ -11,6 +22,7 @@ def run
 
   trap('INT') do # add a trap block to capture the SIGINT signal
     puts 'SIGINT received. Writing processed data to file.'
+    File.write('limit.json', JSON.generate(limit_data))
     File.write('processed.json', JSON.generate(processed_inns))
     exit
   end
@@ -20,17 +32,22 @@ def run
     csv_writer = CsvWriter.new('result')
 
     inns.each do |inn|
+      break if limit_data['request_count'] >= LIMIT_REQUESTS
+
       Fiber.new do
         result = dadata_api.search_by_inn(inn)
         csv_writer.write(result)
         processed_inns << inn
+        limit_data['request_count'] += 1
         Fiber.yield
       end.resume
     end
 
+    File.write('limit.json', JSON.generate(limit_data))
     File.write('processed.json', JSON.generate(processed_inns))
   rescue StandardError => e
     puts e
+    File.write('limit.json', JSON.generate(limit_data))
     File.write('processed.json', JSON.generate(processed_inns))
 
     retry
