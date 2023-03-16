262a53620b92b17258fd395f4e1944181471b022
Dmitriy Tihomirov <dmitriy.tihomirov@okdesk.ru>
2023-03-16 09:51:17 +0300

    new files

--- main.rb
+++ main.rb
@@ -6,24 +6,22 @@ require_relative 'csv_writer'
 LIMIT_REQUESTS = 180000
 
 def run
-  limit_data = JSON.parse(File.read('limit.json'))
-  if limit_data['date'] != Date.today.to_s
-    limit_data['date'] = Date.today.to_s
-    limit_data['request_count'] = 0
+  session_data = JSON.parse(File.read('session_data.json'))
+  if session_data['date'] != Date.today.to_s
+    session_data['date'] = Date.today.to_s
+    session_data['request_count'] = 0
   end
-  if limit_data['request_count'] >= LIMIT_REQUESTS
+  if session_data['request_count'] >= LIMIT_REQUESTS
     return
   end
 
   output_inns = JSON.parse(File.read('output.json'))
-  processed_inns = JSON.parse(File.read('processed.json')) || []
-  inns = output_inns - processed_inns
-  puts processed_inns.count
+  inns = output_inns[output_inns.index(session_data['last_inn'])..output_inns.size]
+  puts inns.size
 
   trap('INT') do # add a trap block to capture the SIGINT signal
     puts 'SIGINT received. Writing processed data to file.'
-    File.write('limit.json', JSON.generate(limit_data))
-    File.write('processed.json', JSON.generate(processed_inns))
+    File.write('session_data.json', JSON.generate(session_data))
     exit
   end
 
@@ -32,23 +30,21 @@ def run
     csv_writer = CsvWriter.new('result')
 
     inns.each do |inn|
-      break if limit_data['request_count'] >= LIMIT_REQUESTS
+      break if session_data['request_count'] >= LIMIT_REQUESTS
 
       Fiber.new do
         result = dadata_api.search_by_inn(inn)
         csv_writer.write(result)
-        processed_inns << inn
-        limit_data['request_count'] += 1
+        session_data['last_inn'] = inn
+        session_data['request_count'] += 1
         Fiber.yield
       end.resume
     end
 
-    File.write('limit.json', JSON.generate(limit_data))
-    File.write('processed.json', JSON.generate(processed_inns))
+    File.write('session_data.json', JSON.generate(session_data))
   rescue StandardError => e
     puts e
-    File.write('limit.json', JSON.generate(limit_data))
-    File.write('processed.json', JSON.generate(processed_inns))
+    File.write('session_data.json', JSON.generate(session_data))
 
     retry
   end
